23c59d05e3dbdc3fb91e2f10f5aa0407050861ba
Alex Louden <alex@louden.com>
2014-05-03 14:42:35 +0800

    Background removal

--- video.py
+++ video.py
@@ -8,39 +8,51 @@ max_yellow = np.array([70, 100, 256])
 min_rocks = np.array([0, 0, 0])
 max_rocks = np.array([180, 30, 256])
 
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+bigkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (35, 35))
+
 
 def main(filename):
 
-    filename = 'bigrock.mp4'
-
     video = cv2.VideoCapture(filename)
-    f, img = video.read()
+    fgbg = cv2.BackgroundSubtractorMOG2(100, 150, True)
+    fgbg2 = cv2.BackgroundSubtractorMOG2(100, 150, False)
 
-    cv2.imshow('raw', img)
+    while(1):
+        playing, frame = video.read()
 
-    img_blur = cv2.GaussianBlur(img, (11, 11), 0)
-    cv2.imshow('blur', img_blur)
-    img_hsv = cv2.cvtColor(img_blur, cv2.COLOR_BGR2HSV)
+        if not playing:
+            return
 
-    img_yellow_bw = cv2.inRange(img_hsv, min_yellow, max_yellow)
-    cv2.imshow('yellow range', img_yellow_bw)
+        # blur image
+        frame = cv2.GaussianBlur(frame, (3, 3), 0)
 
-    img_rocks_bw = cv2.inRange(img_hsv, min_rocks, max_rocks)
-    cv2.imshow('rocks range', img_rocks_bw)
+        fgmask = fgbg.apply(frame)
+        fgmask2 = fgbg2.apply(frame)
 
-    
+        cv2.imshow('raw', frame)
+
+        cv2.imshow('frame', fgmask)
+
+        opened = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+        cv2.imshow('opened frame', opened)
+
+        closed = cv2.morphologyEx(fgmask2, cv2.MORPH_CLOSE, bigkernel)
+        cv2.imshow('closed frame', closed)
+
+        wait()
 
 
 def wait():
     cv2.waitKey()
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     os.chdir('/Users/alex/Projects/rio_video')
-    try:
-        main('bigrock.mp4')
-        wait()
-    except Exception as e:
-        print e
-        wait()
-        raise
+    # try:
+    main('background.mp4')
+    wait()
+    # except Exception as e:
+    #     print e
+        # wait()
+        # raise
