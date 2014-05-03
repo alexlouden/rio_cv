8d08f6601b70a7e6417864bfebde96b6b9a953f6
Alex Louden <alex@louden.com>
2014-05-03 15:34:48 +0800

    Finding largest shadow contour

--- video.py
+++ video.py
@@ -18,6 +18,7 @@ def main(filename):
     fgbg = cv2.BackgroundSubtractorMOG2(100, 150, True)
     fgbg2 = cv2.BackgroundSubtractorMOG2(100, 150, False)
 
+    count = 0
     while(1):
         playing, frame = video.read()
 
@@ -37,10 +38,50 @@ def main(filename):
         opened = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
         cv2.imshow('opened frame', opened)
 
-        closed = cv2.morphologyEx(fgmask2, cv2.MORPH_CLOSE, bigkernel)
-        cv2.imshow('closed frame', closed)
+        get_contour(opened)
 
-        wait()
+        # closed = cv2.morphologyEx(fgmask2, cv2.MORPH_CLOSE, bigkernel)
+        # cv2.imshow('closed frame', closed)
+
+        if count > 100:
+            wait()
+        count += 1
+
+
+def get_contour(mask):
+    # get just grey (127)
+    shadow = cv2.inRange(mask.copy(), 126, 128)
+    shadow_contour = np.zeros(shadow.shape, np.uint8)
+
+    contours, hierarchy = cv2.findContours(
+        shadow,
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+    print len(contours)
+
+    if not contours:
+        return
+
+    # Find large contours
+    contour_sizes = [
+        (cv2.contourArea(contour), contour)
+        for contour in contours
+    ]
+
+    max_contour = max(contour_sizes, key=lambda x: x[0])
+    contour_size, largest_contour = max_contour
+
+    print contour_size
+
+    if contour_size > 1000:
+        # fill largest contour
+        # import ipdb; ipdb.set_trace()
+        cv2.drawContours(shadow_contour, [largest_contour], 0, 255, -1)
+
+    cv2.imshow('shadow contour', shadow_contour)
+
+    return shadow_contour
 
 
 def wait():
